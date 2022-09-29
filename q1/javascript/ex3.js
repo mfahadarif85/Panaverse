f8a108bcae3effde3492b02a12180aaa43515e2a
mfahadarif85 <mfahadarif@gmail.com>
2022-09-29 17:57:32 +0500

    added q desc

--- q1/javascript/ex3.js
+++ q1/javascript/ex3.js
@@ -1,3 +1,5 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
 let userName = "Muhammad Fahad ARIF";
 function titleCased(str) {
   return str
