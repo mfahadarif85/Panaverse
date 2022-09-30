9e540d7595846a621a4efe2ec04e1d185c1940e7
mfahadarif85 <mfahadarif@gmail.com>
2022-09-30 09:44:45 +0500

    added qs desc

---
+++ q1/javascript/ex2-10/ex9.js
@@ -0,0 +1,7 @@
+/* Favorite Number: Store your favorite number in a variable. 
+Then, using that variable, create a message that reveals your favorite number. 
+Print that message. */
+
+const favNum = 85;
+const message = `My favourite number is ${favNum}.`;
+console.log(message);
