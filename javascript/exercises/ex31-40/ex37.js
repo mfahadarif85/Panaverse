9c1ca44d1005eb745c40dd467fb3c94227a165f7
mfahadarif85 <mfahadarif@gmail.com>
2022-10-02 22:35:31 +0500

    till ex39

--- javascript/exercises/ex31-40/ex37.js
+++ javascript/exercises/ex31-40/ex37.js
@@ -1,1 +1,10 @@
-/* Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love JavaScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message. */
+/* Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love JavaScript.
+ Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message. */
+
+function make_shirt(size = "large", message = "I love JavaScript") {
+  console.log(`Shirt size: ${size}; Shirt print text: ${message}`);
+}
+
+make_shirt();
+make_shirt("medium");
+make_shirt("small", "Pakistan Zindabad");
